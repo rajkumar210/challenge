fc66a72012f0191e223cf1cf91f04c9fe860173d
rajkumar210 <76163751+rajkumar210@users.noreply.github.com>
2022-08-06 13:27:24 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import joblib
+model = joblib.load('Spam Ham Detector(1)')
+st.title('SPAM HAM DETECTOR')
+ip = st.text_input("Enter your message")
+op = model.predict([ip])
+if st.button('Detect'):
+  st.title(op[0])
